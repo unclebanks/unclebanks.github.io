b1223a2d92bf91e2dea7f225cdf75b0843d3967f
Jessica Chen <jessica.scoltock@protonmail.com>
2021-03-02 00:27:03 +0000

    Add some documentation about npm + webpack

---
+++ guides/webpack.md
@@ -0,0 +1,5 @@
+The [webpack documentation](https://webpack.js.org/guides/) has some comprehensive guides.
+
+I mostly followed the [SurviveJS Webpack](https://survivejs.com/webpack/foreword/) guide, which was easier to understand.
+
+For a quick overview, [this guide](https://www.valentinog.com/blog/webpack/) from Valentino Gagliardi gives you the bare bones to be up to speed on webpack while being as comprehensive as possible
--- webpack.config.js
+++ webpack.config.js
@@ -8,24 +8,39 @@ const path = require('path');
 const outputPath = path.resolve(__dirname, folder || 'dist');
 
 module.exports = {
+    // watch files for changes if we are in dev mode
     watch: mode === 'development',
+
+    // Get the index.js file and collect all of the js it imports
     entry: ['./src', 'webpack-plugin-serve/client'],
+
+    // Remove old files before generating more
     output: { clean: true, path: outputPath },
     mode,
+
+    // Custom handling for certain file types
     module: {
         rules: [
+            // Loaders to handle css files
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
         ],
     },
+
+    // Extra behaviour
     plugins: [
+        // Use some html we have written
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+
+        // Copy everything in ./src/resources into the output folder
         new CopyPlugin({
             patterns: [
                 path.resolve(__dirname, 'src', 'resources'),
             ],
         }),
+
+        // Live reloading of the page on code change
         new WebpackPluginServe({
             port: process.env.NNPG_PORT || 3000,
             static: outputPath,
