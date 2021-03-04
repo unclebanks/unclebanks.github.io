54d707c60067421a5814fb93597a22201d84563e
Jessica Chen <jessica.scoltock@protonmail.com>
2021-03-04 20:57:12 +0000

    Add separating blank lines to SettingsModal template

--- src/components/modals/SettingsModal.vue
+++ src/components/modals/SettingsModal.vue
@@ -3,11 +3,11 @@
     name="settings"
     title="Settings"
   >
-    <template
-      #body
-    >
+    <template #body>
       <h4>Visual Settings</h4>
+
       <p>Choose which sprite You want to display for your pokemon:</p>
+
       <p>
         <span class="spriteChoice">
           <input
@@ -16,6 +16,7 @@
             name="spriteChoice"
             @change="ui.changeSpriteChoice()"
           ><label for="spriteChoiceBack">Back</label>
+
           <input
             id="spriteChoiceFront"
             type="radio"
@@ -24,7 +25,9 @@
           ><label for="spriteChoiceFront">Front</label>
         </span>
       </p>
+
       <h4>Console Settings</h4>
+
       <p>
         <input
           id="enableConsole"
@@ -32,42 +35,52 @@
           checked="true"
         ><span>Enable console</span>
       </p>
+
       <Button
         text="Clear Console"
         classes="is-warning"
         @click="ui.clearConsole()"
       />
+
       <h4>Import and Export Save</h4>
+
       <div class="buttons">
         <Button
           text="Force Save"
           classes="is-primary"
           @click="ui.forceSave()"
         />
+
         <span
           id="forceSave"
           style="color:red"
         >Saved!</span>
+
         <Button
           text="Export"
           classes="is-primary"
           @click="ui.exportSaveDialog()"
         />
+
         <Button
           text="Import"
           classes="is-primary"
           @click="ui.importSaveDialog()"
         />
       </div>
+
       <h4>Clear Game Data</h4>
+
       <p style="color:red;font-weight:bold">
         Warning! This cannot be undone.
       </p>
+
       <p>
         <input
           id="confirmClearData"
           type="checkbox"
         >
+
         <Button
           text="Clear game data"
           classes="is-danger"
