076313971a10c31c8411d8269d7bad78d7ab145e
Jessica Chen <jessica.scoltock@protonmail.com>
2021-03-04 21:22:02 +0000

    Move game component imports to the App component

--- src/app.js
+++ src/app.js
@@ -1,25 +1,8 @@
 import { createApp } from 'vue';
 import App from './components/App.vue';
-import PokemonList from './components/PokemonList.vue';
-import RoutesBox from './components/RoutesBox.vue';
-import ViewSelect from './components/ViewSelect.vue';
-import Inventory from './components/Inventory.vue';
-import EnemyBox from './components/EnemyBox.vue';
-import PlayerBox from './components/PlayerBox.vue';
-import Console from './components/Console.vue';
-import NavBox from './components/NavBox.vue';
 
 export default () => {
     const app = createApp(App);
-
-    app.component('PokemonList', PokemonList);
-    app.component('RoutesBox', RoutesBox);
-    app.component('ViewSelect', ViewSelect);
-    app.component('Inventory', Inventory);
-    app.component('EnemyBox', EnemyBox);
-    app.component('PlayerBox', PlayerBox);
-    app.component('Console', Console);
-    app.component('NavBox', NavBox);
 
     return app.mount('#app');
 };
