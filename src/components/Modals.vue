d2fc8e6bf668d33415f4bff8fc46e85f5aa570ee
PixLSteam <PixLSteam@users.noreply.github.com>
2021-03-07 17:44:19 +0100

    Vitamin Modal

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -1,6 +1,7 @@
 <template>
   <SaveTextModal :ui="ui" />
   <InventoryModal />
+  <VitaminModal />
   <SettingsModal :ui="ui" />
   <AchievementsModal />
   <StatisticsModal />
@@ -11,6 +12,7 @@
 <script>
 import SaveTextModal from './modals/SaveTextModal.vue';
 import InventoryModal from './modals/InventoryModal.vue';
+import VitaminModal from './modals/VitaminModal.vue';
 import SettingsModal from './modals/SettingsModal.vue';
 import AchievementsModal from './modals/AchievementsModal.vue';
 import StatisticsModal from './modals/StatisticsModal.vue';
@@ -21,6 +23,7 @@ export default {
     components: {
         SaveTextModal,
         InventoryModal,
+        VitaminModal,
         SettingsModal,
         AchievementsModal,
         StatisticsModal,
