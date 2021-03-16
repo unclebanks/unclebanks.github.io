8ff2ef3a79aebda736d82fc615b9385d11c279a2
banks92 <49108377+unclebanks@users.noreply.github.com>
2021-03-15 23:04:22 -0700

    Update PokemonList.vue

--- src/components/PokemonList/PokemonList.vue
+++ src/components/PokemonList/PokemonList.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="listBox">
-    <PokeStorage :ui="ui" />
+    <PokeStorage />
   </div>
 </template>
 
@@ -10,10 +10,6 @@ import PokeStorage from './PokeStorage.vue';
 export default {
     components: {
         PokeStorage,
-    },
-
-    props: {
-        ui: { type: Object, required: true },
     },
 };
 </script>
