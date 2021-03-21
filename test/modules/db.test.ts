ea7f33bb62f50180018bf5317fbf137ba0fade31
Jessica Chen <jessica.scoltock@protonmail.com>
2021-03-21 17:48:17 +0000

    Add tests for pokemon image files

---
+++ test/modules/db.test.ts
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import POKEDEX, { PokemonNameType } from '../../src/modules/db';
+
+const fileExists = async (path) => Boolean(await fs.promises.stat(path).catch(() => false));
+
+// move this into some src file and import it
+const imagePath = (
+    type: 'normal' | 'shiny',
+    side: 'front' | 'back',
+    name: PokemonNameType,
+) => `assets/sprites/${type}/${side}/${name}.png`;
+
+describe('Pokedex', () => {
+    describe('All pokemon', () => {
+        describe('have images', () => {
+            const types = ['normal', 'shiny'];
+            const sides = ['front', 'back'];
+            const testImage = (type, side) => (poke) => {
+                const name = poke.name;
+                const image = `./src/resources/${imagePath(type, side, poke.name)}`;
+                test(name, () => fileExists(image).then((data) => {
+                    expect(data).toBe(true);
+                }));
+            };
+            types.forEach((type) => {
+                sides.forEach((side) => {
+                    describe(type, () => {
+                        describe(side, () => {
+                            POKEDEX.forEach(testImage(type, side));
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
