c4ad9c91853076f47f80bb2f8d6249272fc5f578
Jessica Chen <jessica.scoltock@protonmail.com>
2021-03-22 00:02:40 +0000

    Move some config files into a separate folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,25 @@ const { merge } = require('webpack-merge');
 const {
     mode, folder, host, port,
 } = require('webpack-nano/argv');
-const { getCommon } = require('./webpack/common');
+const { getCommon } = require('./.config/webpack/common');
 
 const outputPath = path.resolve(__dirname, folder || 'dist');
 const sourcePath = path.resolve(__dirname, 'src');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const nodeCachePath = path.resolve(nodeModulesPath, '.cache');
+const configPath = path.resolve(__dirname, '.config');
 const options = {
     outputPath,
     sourcePath,
     nodeModulesPath,
     nodeCachePath,
+    configPath,
     host,
     port,
 };
 const getModeConfig = mode === 'development'
-    ? require('./webpack/development')
-    : require('./webpack/production');
+    ? require('./.config/webpack/development')
+    : require('./.config/webpack/production');
 
 const commonConfig = getCommon(options);
 const modeConfig = getModeConfig(options);
