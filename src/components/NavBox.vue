c0f07c22e043b6a00797a90435dcae06f801629b
Penguindude2000 <80924419+Penguindude2000@users.noreply.github.com>
2021-03-24 19:04:53 -0500

    update layout.js

--- src/components/NavBox.vue
+++ src/components/NavBox.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="navBox">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <div class="buttons is-centered mb-0">
       <acheivementButton
         onclick="userInteractions.viewAchievements()"
+        class="button"
       >
         .
       </acheivementButton>
       <storageButton
         onclick="userInteractions.viewInventory()"
+        class="button"
       >
         .
       </storageButton>
 
       <statsButton
         onclick="userInteractions.viewStatistics()"
+        class="button"
       >
         .
       </statsButton>
@@ -21,6 +29,7 @@
     <div class="buttons is-centered">
       <pokedexButton
         id="viewPokeDex"
+        onclick="userInteractions.viewStatistics()"
         class="button"
       >
         .
