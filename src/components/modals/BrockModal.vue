481857e66736171d33d4774e89313c1f75a1dc92
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-04-12 21:00:05 -0700

    Quick update

--- src/components/modals/BrockModal.vue
+++ src/components/modals/BrockModal.vue
@@ -8,11 +8,15 @@
         <div id="brockInfo">
           <h4>Brock</h4><br>
           <div id="brockImage" />
-          <br>
-          <div id="brockPokemon">
-            <h4>Brock's Team</h4>
-            <div id="brockMon1">
-              Geodude
+          <h2>Information</h2>
+          <p>Brock is the PEWTER CITY Gym Leader who specializes in ROCK type POKEMON</p>
+          <div id="brockInformation">
+            <div id="brockPokemon">
+              <h4>Brock's Team</h4>
+              <p id="brockMon1" />
+              <p>Geodude</p>
+              <p id="brockMon2" />
+              <p>Onix</p>
             </div>
           </div>
         </div>
