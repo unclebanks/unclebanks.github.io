a5c90b7d9ce4a910e15faceae3e970b2a062a52d
unclebanks <49108377+unclebanks@users.noreply.github.com>
2021-04-22 00:54:54 -0700

    Quick Cleanup

--- src/components/PokeGear.vue
+++ src/components/PokeGear.vue
@@ -1,35 +1,7 @@
 <template>
   <div id="pokeGear">
     <div id="inventoryBox">
-      <span>Inventory</span><br>
-      <button
-        id="badgesButton"
-        onclick="userInteractions.viewBadgeCase()"
-        class="button"
-      >
-        Badges
-      </button>
-      <button
-        id="evoStonesButton"
-        onclick="userInteractions.viewEvoStones()"
-        class="button"
-      >
-        Evo Stones
-      </button>
-      <button
-        id="keyItemsButton"
-        onclick="userInteractions.viewKeyItems()"
-        class="button"
-      >
-        Key Items
-      </button><br>
-      <button
-        id="keyItemsButton"
-        onclick="userInteractions.enterCode()"
-        class="button"
-      >
-        Secret Codes
-      </button>
+      <span>EMPTY</span><br>
     </div>
   </div>
 </template>
