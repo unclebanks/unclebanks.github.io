7d8f3d469b578dfec706aea2cf40cba8b755fca8
banks92 <49108377+unclebanks@users.noreply.github.com>
2021-08-17 19:54:30 -0700

    temporarily revert pokeball/currency changes
    ^^^

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,74 +1,77 @@
 <template>
   <div id="invBox">
-    <div id="playerItems">
-      <p>
-        Catch: <label><input
-          id="enableCatchAll"
-          type="checkbox"
-        ><span>All</span></label> <label><input
-          id="enableCatchNew"
-          type="checkbox"
-        ><span>New</span></label>
-      </p>
-      <div id="playerPokeBalls">
-        <div
-          id="inventoryPokeBallAmount"
-        >
-          <label for="pokeball"><span title="Pokeball"><img
-            class="pokeballImg ballIcon"
-            src="assets/images/pokeballs/pokeball.png"
-            onclick="userInteractions.changeSelectedBall('pokeball')"
-          ></span><br><span class="ball-amount pokeball">10</span></label>
-        </div>
-        <div
-          id="inventoryGreatBallAmount"
-        >
-          <label for="greatball"><span title="Greatball"><img
-            class="greatballImg ballIcon"
-            src="assets/images/pokeballs/greatball.png"
-            onclick="userInteractions.changeSelectedBall('greatball')"
-          ></span><br><span class="ball-amount greatball">0</span></label><br>
-        </div>
-        <div
-          id="inventoryUltraBallAmount"
-        >
-          <label for="ultraball"><span title="Ultraball"><img
-            class="ultraballImg ballIcon"
-            src="assets/images/pokeballs/ultraball.png"
-            onclick="userInteractions.changeSelectedBall('ultraball')"
-          ></span><br><span class="ball-amount ultraball">0</span></label><br>
-        </div>
-        <div
-          id="inventoryMasterBallAmount"
-        >
-          <label for="masterball"><span title="Masterball"><img
-            class="masterballImg ballIcon"
-            src="assets/images/pokeballs/masterball.png"
-            onclick="userInteractions.changeSelectedBall('masterball')"
-          ></span><br><span class="ball-amount masterball">0</span></label><br>
-        </div>
-      </div>
-      <div id="playerCurrencyAmounts">
-        <div id="inventoryPokeCoinsAmount">
-          <span title="PokeCoins"><img
-            class="pokecoins coinIcon"
-            src="assets/images/currency/PokeCoin.png"
-          ></span><br><span id="pokeCoins">0</span><br>
-        </div>
-        <div id="inventoryCatchCoinsAmount">
-          <span title="CatchCoins"><img
-            class="catchcoins coinIcon"
-            src="assets/images/currency/CatchCoin.png"
-          ></span><br><span id="catchCoins">0</span><br>
-        </div>
-        <div id="inventoryBattleCoinsAmount">
-          <span title="BattleCoins"><img
-            class="battlecoins coinIcon"
-            src="assets/images/currency/BattleCoin.png"
-          ></span><br><span id="battleCoins">0</span><br>
-        </div>
-      </div>
-    </div>
+    <p>
+      Catch: <label><input
+        id="enableCatchAll"
+        type="checkbox"
+      ><span>All</span></label> <label><input
+        id="enableCatchNew"
+        type="checkbox"
+      ><span>New</span></label>
+    </p>
+    <input
+      id="pokeball"
+      type="radio"
+      name="name"
+      value="pokeball"
+      checked="true"
+      onclick="userInteractions.changeSelectedBall('pokeball')"
+    ><label for="pokeball"><span title="Pokeball"><img
+      class="pokeballImg ballIcon"
+      src="assets/images/pokeballs/pokeball.png"
+    ></span>Pokeballs: <span class="ball-amount pokeball">10</span></label><br>
+    <input
+      id="greatball"
+      type="radio"
+      name="name"
+      value="greatball"
+      onclick="userInteractions.changeSelectedBall('greatball')"
+    ><label for="greatball"><span title="Greatball"><img
+      class="greatballImg ballIcon"
+      src="assets/images/pokeballs/greatball.png"
+    ></span>Greatballs: <span class="ball-amount greatball">0</span></label><br>
+    <input
+      id="ultraball"
+      type="radio"
+      name="name"
+      value="ultraball"
+      onclick="userInteractions.changeSelectedBall('ultraball')"
+    ><label for="ultraball"><span title="Ultraball"><img
+      class="UltraballImg ballIcon"
+      src="assets/images/pokeballs/ultraball.png"
+    ></span>Ultraballs: <span class="ball-amount ultraball">0</span></label><br>
+    <input
+      id="masterball"
+      type="radio"
+      name="name"
+      value="ultraball"
+      onclick="userInteractions.changeSelectedBall('masterball')"
+    ><label for="masterball"><span title="Masterball"><img
+      class="MasterballImg ballIcon"
+      src="assets/images/pokeballs/masterball.png"
+    ></span>Masterballs: <span class="ball-amount masterball">0</span></label><br>
+    <span>Currency:</span><br>
+    <span
+      id="pokeC"
+      title="PokeCoins"
+    ><img
+      class="pokecoins coinIcon"
+      src="assets/images/currency/PokeCoin.png"
+    ></span> PokeCoins: <span id="pokeCoins">0</span><br>
+    <span
+      id="catchC"
+      title="CatchCoins"
+    ><img
+      class="catchcoins coinIcon"
+      src="assets/images/currency/CatchCoin.png"
+    ></span> CatchCoins: <span id="catchCoins">0</span><br>
+    <span
+      id="battleC"
+      title="BattleCoins"
+    ><img
+      class="battlecoins coinIcon"
+      src="assets/images/currency/BattleCoin.png"
+    ></span> BattleCoins: <span id="battleCoins">0</span><br><br>
     <div id="regionSelect">
       <button
         id="kantoButton"
