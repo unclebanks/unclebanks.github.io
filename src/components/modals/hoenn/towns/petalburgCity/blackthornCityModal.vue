d2effb6277fc0c0276c233e9faa8f82e1041d326
banks92 <49108377+unclebanks@users.noreply.github.com>
2021-08-19 19:02:34 -0700

    Hoenn Update
    * Added template region stuff (needs renaming cba to do so right now)
    * Changed modal closing on gym battle
    * More regions made accessible (idle only)

---
+++ src/components/modals/hoenn/towns/petalburgCity/blackthornCityModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <CardModal
+    name="blackthorncity"
+    title="Blackthorn City"
+  >
+    <template #body>
+      <img
+        id="blackthornCityModalBox"
+        usemap="#blackthornCityMap"
+      >
+
+      <map
+        name="blackthornCityMap"
+      >
+        <area
+          shape="rect"
+          coords="0,0,0,0"
+          onclick="userInteractions.test()"
+        >
+      </map>
+    </template>
+  </CardModal>
+</template>
+
+<script>
+import CardModal from '../../../../common/CardModal.vue';
+
+export default {
+    components: {
+        CardModal,
+    },
+};
+</script>
